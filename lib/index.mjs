KWierso/proposal-temporal

Fix bugs in setup().

polyfill/lib/index.mjs
@@ -3,13 +3,23 @@ import * as Intl from './intl.mjs';
 
 export { Temporal, Intl };
 export function setup(global = globalThis) {
-  global.Temporal = {};
+  Object.defineProperty(global, "Temporal", {
+    value: {},
+    writable: true,
+    enumerable: false,
+    configurable: true,
+  });
   copy(global.Temporal, Temporal);
   copy(global.Intl, Intl);
 
   function copy(target, source) {
     for (const prop of Object.getOwnPropertyNames(source)) {
-      target[prop] = source[prop];
+      Object.defineProperty(target, prop, {
+        value: source[prop],
+        writable: true,
+        enumerable: false,
+        configurable: true,
+      });
     }
   }
 }

js-temporal/temporal-polyfill

lib/index.mjs
@@ -1,5 +1,24 @@
 import * as Temporal from './temporal.mjs';
 import * as Intl from './intl.mjs';
 
-globalThis.Temporal = Temporal;
+Object.defineProperty(globalThis, "Temporal", {
+  value: {},
+  writable: true,
+  enumerable: false,
+  configurable: true,
+});
+copy(globalThis.Temporal, Temporal);
+copy(globalThis.Intl, Intl);
+
+function copy(target, source) {
+  for (const prop of Object.getOwnPropertyNames(source)) {
+    Object.defineProperty(target, prop, {
+      value: source[prop],
+      writable: true,
+      enumerable: false,
+      configurable: true,
+    });
+  }
+}
+
 export { Temporal, Intl };