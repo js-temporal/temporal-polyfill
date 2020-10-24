f07b0304e2df39bc2793a471fda0f6c43fce25c9
Philip Chimento <pchimento@igalia.com>
2020-10-24 11:34:53 -0700

    Rename difference() to since()
    We may add a separate method until() for the other direction as part of
    this ticket as well, but in either case this rename needs to be done.
    
    See: #998

--- test/Date/prototype/since/prop-desc.js
+++ test/Date/prototype/since/prop-desc.js
@@ -2,17 +2,17 @@
 // This code is governed by the BSD license found in the LICENSE file.
 
 /*---
-esid: sec-temporal.date.prototype.difference
+esid: sec-temporal.date.prototype.since
 includes: [propertyHelper.js]
 ---*/
 
 assert.sameValue(
-  typeof Temporal.Date.prototype.difference,
+  typeof Temporal.Date.prototype.since,
   "function",
-  "`typeof Date.prototype.difference` is `function`"
+  "`typeof Date.prototype.since` is `function`"
 );
 
-verifyProperty(Temporal.Date.prototype, "difference", {
+verifyProperty(Temporal.Date.prototype, "since", {
   writable: true,
   enumerable: false,
   configurable: true,
