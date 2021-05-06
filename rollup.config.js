f68cbde3e4b2a80cace40886351ef7a75267162f
Philip Chimento <philip.chimento@gmail.com>
2021-05-06 12:10:55 -0700

    Remove any remaining test262, coverage, and cookbook bits

--- rollup.config.js
+++ rollup.config.js
@@ -6,31 +6,27 @@ import { terser } from 'rollup-plugin-terser';
 import { env } from 'process';
 
 const isProduction = env.NODE_ENV === 'production';
-const isTest262 = !!env.TEST262;
 const libName = 'temporal';
-const babelConfig = {
-  exclude: 'node_modules/**',
-  babelHelpers: 'bundled',
-  presets: [
-    [
-      '@babel/preset-env',
-      {
-        targets: '> 0.25%, not dead'
-      }
-    ]
-  ]
-};
-const replaceConfig = { exclude: 'node_modules/**' };
-const resolveConfig = { preferBuiltins: false };
 
 export default [
-  !isTest262 && {
+  {
     input: 'lib/index.mjs',
     plugins: [
-      replace({ ...replaceConfig, __debug__: !isProduction }),
+      replace({ exclude: 'node_modules/**', __debug__: !isProduction }),
       commonjs(),
-      resolve(resolveConfig),
-      babel(babelConfig),
+      resolve({ preferBuiltins: false }),
+      babel({
+        exclude: 'node_modules/**',
+        babelHelpers: 'bundled',
+        presets: [
+          [
+            '@babel/preset-env',
+            {
+              targets: '> 0.25%, not dead'
+            }
+          ]
+        ]
+      }),
       isProduction && terser()
     ],
     output: [
@@ -47,25 +43,5 @@ export default [
         sourcemap: true
       }
     ]
-  },
-  {
-    input: 'lib/shim.mjs',
-    plugins: [replace({ ...replaceConfig, __debug__: false }), commonjs(), resolve(resolveConfig)],
-    output: {
-      name: libName,
-      file: 'script.js',
-      format: 'iife',
-      sourcemap: true
-    }
-  },
-  !isTest262 && {
-    input: 'lib/shim.mjs',
-    output: {
-      name: libName,
-      file: '../out/docs/playground.js',
-      format: 'umd',
-      sourcemap: true
-    },
-    plugins: [replace({ ...replaceConfig, __debug__: true }), commonjs(), resolve(resolveConfig), babel(babelConfig)]
   }
-].filter(Boolean);
+];
