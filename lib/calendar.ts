7ebc700ea92d660f42b6397f7e400122630c2e76
Cam Tenny <cam@igalia.com>
2021-08-19 17:46:13 -0700

    Validate input to Calendar.prototype.fields
    As discussed in #1610, we restrict values returned by the iterator to be no more than the ten known values, prohibiting duplicates. The check for String values has been removed as all of those values are Strings, so the value restriction is plenty.
    
    This does not address the question of return value validation.

--- lib/calendar.ts
+++ lib/calendar.ts
@@ -61,8 +61,22 @@ export class Calendar implements Temporal.Calendar {
   fields(fields) {
     if (!ES.IsTemporalCalendar(this)) throw new TypeError('invalid receiver');
     const fieldsArray = [];
+    const allowed = new Set([
+      'year',
+      'month',
+      'monthCode',
+      'day',
+      'hour',
+      'minute',
+      'second',
+      'millisecond',
+      'microsecond',
+      'nanosecond'
+    ]);
     for (const name of fields) {
       if (ES.Type(name) !== 'String') throw new TypeError('invalid fields');
+      if (!allowed.has(name)) throw new RangeError(`invalid field name ${name}`);
+      allowed.delete(name);
       ArrayPrototypePush.call(fieldsArray, name);
     }
     return impl[GetSlot(this, CALENDAR_ID)].fields(fieldsArray);
