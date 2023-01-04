df0cdbdef91525bcf40c7bd0dd595953d8a75b43
Justin Grant <justingrant@users.noreply.github.com>
2023-01-04 14:32:07 -0800

    CI Tests for Node 18 and 19
    * Add matrix test runs for both demitasse and Test262 for node versions
      14, 16, 18, and 19.
    * Update other CI actions to use Node 19
    * Adapt to newer node versions' requirement that relative imports end
      with ".js", which babel automatically does but TSC (used in demitasse
      tests) does not.
    * Add expected Test262 failures for pre-v19 node versions.

--- runtest262.mjs
+++ runtest262.mjs
@@ -8,6 +8,10 @@ if (isProduction) {
   expectedFailureFiles.push(isTranspiledBuild ? 'test/expected-failures-es5.txt' : 'test/expected-failures-opt.txt');
 }
 
+const nodeVersion = parseInt(process.versions.node.split('.')[0]);
+if (nodeVersion < 18) expectedFailureFiles.push('test/expected-failures-before-node18.txt');
+if (nodeVersion < 16) expectedFailureFiles.push('test/expected-failures-before-node16.txt');
+
 const result = runTest262({
   test262Dir: 'test262',
   polyfillCodeFile: 'dist/script.js',
