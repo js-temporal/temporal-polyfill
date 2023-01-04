f78e1c692d70d1479b6ba44e5f70cfc3ed8b6425
Justin Grant <justingrant@users.noreply.github.com>
2023-01-04 14:32:07 -0800

    CI Tests for Node 18 and 19
    * Add matrix test runs for both demitasse and Test262 for node versions
      14, 16, 18, and 19.
    * Update other CI actions to use Node 19
    * Adapt to newer node versions' requirement that relative imports end
      with ".js", which babel automatically does but TSC (used in demitasse
      tests) does not.
    * Add expected Test262 failures for pre-v19 node versions.

--- test/resolve.source.mjs
+++ test/resolve.source.mjs
@@ -5,11 +5,19 @@
 
 import fs from 'fs';
 const PKG = JSON.parse(fs.readFileSync('package.json', { encoding: 'utf-8' }));
-export function resolve(specifier, parent, defaultResolve) {
+export function resolve(specifier, context, defaultResolve) {
   if (specifier === PKG.name) {
     specifier = new URL('../tsc-out/temporal.js', import.meta.url).toString();
   } else if (specifier.startsWith('../lib')) {
     specifier = new URL(specifier.replace('../lib/', '../tsc-out/'), import.meta.url).toString();
+    if (!specifier.endsWith('.js')) specifier += '.js';
+  } else if (
+    context.parentURL &&
+    context.parentURL.includes('/tsc-out/') &&
+    specifier.startsWith('./') &&
+    !specifier.endsWith('.js')
+  ) {
+    specifier += '.js';
   }
-  return defaultResolve(specifier, parent);
+  return defaultResolve(specifier, context);
 }
