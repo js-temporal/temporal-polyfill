79b32a20e3f1be94190ab1ff71b8000aaed031c8
James Wright <jameswr@google.com>
2023-01-17 16:04:32 -0800

    Parameterize test262 config.
    This will allow it to be re-used over in the runner repo for basic CI
    testing.

---
+++ test262config.mjs
@@ -0,0 +1,25 @@
+export function runTest262(runTest262Callback) {
+  const isProduction = process.env.NODE_ENV === 'production';
+  const isTranspiledBuild = !!process.env.TRANSPILE;
+
+  const expectedFailureFiles = ['test/expected-failures.txt'];
+  if (isProduction) {
+    expectedFailureFiles.push(isTranspiledBuild ? 'test/expected-failures-es5.txt' : 'test/expected-failures-opt.txt');
+  }
+
+  const nodeVersion = parseInt(process.versions.node.split('.')[0]);
+  if (nodeVersion < 18) expectedFailureFiles.push('test/expected-failures-before-node18.txt');
+  if (nodeVersion < 16) expectedFailureFiles.push('test/expected-failures-before-node16.txt');
+
+  // As we migrate commits from proposal-temporal, remove expected failures from here.
+  expectedFailureFiles.push('test/expected-failures-todo-migrated-code.txt');
+
+  const result = runTest262Callback({
+    test262Dir: 'test262',
+    polyfillCodeFile: 'dist/script.js',
+    expectedFailureFiles,
+    testGlobs: process.argv.slice(2)
+  });
+
+  process.exit(result ? 0 : 1);
+}
