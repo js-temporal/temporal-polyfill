1b768aaa0e73b53d2cdb2ab7dc0e4896ca8e018c
James Wright <jameswr@google.com>
2023-02-01 14:57:39 -0800

    Use a command line parser in runtest262.

--- runtest262.mjs
+++ runtest262.mjs
@@ -1,25 +1,47 @@
 import runTest262 from '@js-temporal/temporal-test262-runner';
+import yargs from 'yargs';
+import * as process from 'process';
+import { hideBin } from 'yargs/helpers';
 
 const isProduction = process.env.NODE_ENV === 'production';
 const isTranspiledBuild = !!process.env.TRANSPILE;
 
-const expectedFailureFiles = ['test/expected-failures.txt'];
-if (isProduction) {
-  expectedFailureFiles.push(isTranspiledBuild ? 'test/expected-failures-es5.txt' : 'test/expected-failures-opt.txt');
-}
+yargs(hideBin(process.argv))
+  .command(
+    '*',
+    'Run test262 tests',
+    (builder) => {
+      builder.option('update-expected-failure-files', {
+        requiresArg: false,
+        default: false,
+        type: 'boolean',
+        description: 'Whether to update the existing expected-failure files on-disk and remove tests that now pass.'
+      });
+    },
+    (parsedArgv) => {
+      const expectedFailureFiles = ['test/expected-failures.txt'];
+      if (isProduction) {
+        expectedFailureFiles.push(
+          isTranspiledBuild ? 'test/expected-failures-es5.txt' : 'test/expected-failures-opt.txt'
+        );
+      }
 
-const nodeVersion = parseInt(process.versions.node.split('.')[0]);
-if (nodeVersion < 18) expectedFailureFiles.push('test/expected-failures-before-node18.txt');
-if (nodeVersion < 16) expectedFailureFiles.push('test/expected-failures-before-node16.txt');
+      const nodeVersion = parseInt(process.versions.node.split('.')[0]);
+      if (nodeVersion < 18) expectedFailureFiles.push('test/expected-failures-before-node18.txt');
+      if (nodeVersion < 16) expectedFailureFiles.push('test/expected-failures-before-node16.txt');
 
-// As we migrate commits from proposal-temporal, remove expected failures from here.
-expectedFailureFiles.push('test/expected-failures-todo-migrated-code.txt');
+      // As we migrate commits from proposal-temporal, remove expected failures from here.
+      expectedFailureFiles.push('test/expected-failures-todo-migrated-code.txt');
 
-const result = runTest262({
-  test262Dir: 'test262',
-  polyfillCodeFile: 'dist/script.js',
-  expectedFailureFiles,
-  testGlobs: process.argv.slice(2)
-});
+      const result = runTest262({
+        test262Dir: 'test262',
+        polyfillCodeFile: 'dist/script.js',
+        expectedFailureFiles,
+        testGlobs: parsedArgv._,
+        updateExpectedFailureFiles: parsedArgv.updateExpectedFailureFiles
+      });
 
-process.exit(result ? 0 : 1);
+      process.exit(result ? 0 : 1);
+    }
+  )
+  .help().argv;
