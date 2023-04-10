fa4820acfa3c6e30640e72e5b1a05fdbb3082f4d
James Wright <jameswr@google.com>
2023-04-10 08:04:53 -0700

    Simplify some redundant BigInt operations. (#234)

--- lib/legacydate.ts
+++ lib/legacydate.ts
@@ -1,11 +1,10 @@
 import { Instant } from './instant';
 
 import JSBI from 'jsbi';
-import * as ES from './ecmascript';
 import { MILLION } from './ecmascript';
 
 export function toTemporalInstant(this: Date) {
   // Observable access to valueOf is not correct here, but unavoidable
   const epochNanoseconds = JSBI.multiply(JSBI.BigInt(+this), MILLION);
-  return new Instant(ES.ToBigInt(epochNanoseconds));
+  return new Instant(epochNanoseconds);
 }
