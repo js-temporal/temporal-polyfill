abd392d9840a77dd758e6b5dd2953192895547fe
Justin Grant <justingrant@users.noreply.github.com>
2023-06-12 21:28:30 -0700

    Refactor time zone identifier handling
    UPSTREAM_COMMIT=076f2871a8b91d78cb90aa0012ea55fdb4d94580

--- test/ecmascript.mjs
+++ test/ecmascript.mjs
@@ -5,11 +5,13 @@ import Pretty from '@pipobscure/demitasse-pretty';
 const { reporter } = Pretty;
 
 import { strict as assert } from 'assert';
-const { deepEqual, throws } = assert;
+const { deepEqual, throws, equal } = assert;
 
 import * as ES from '../lib/ecmascript';
 import { GetSlot, TIMEZONE_ID } from '../lib/slots';
 import { TimeZone } from '../lib/timezone';
+
+import { readFileSync } from 'fs';
 
 describe('ECMAScript', () => {
   describe('GetNamedTimeZoneDateTimeParts', () => {
@@ -444,6 +446,61 @@ describe('ECMAScript', () => {
       [null, 1, 'hello', true, Symbol('1'), 1n].forEach((options) =>
         throws(() => ES.GetOptionsObject(options), TypeError)
       );
+    });
+  });
+
+  describe('GetAvailableNamedTimeZoneIdentifier', () => {
+    it('Case-normalizes time zone IDs', () => {
+      // eslint-disable-next-line max-len
+      // curl -s https://raw.githubusercontent.com/unicode-org/cldr-json/main/cldr-json/cldr-bcp47/bcp47/timezone.json > cldr-timezone.json
+      const cldrTimeZonePath = new URL('./cldr-timezone.json', import.meta.url);
+      const cldrTimeZoneJson = JSON.parse(readFileSync(cldrTimeZonePath));
+
+      // get CLDR's time zone IDs
+      const cldrIdentifiers = Object.entries(cldrTimeZoneJson.keyword.u.tz)
+        .filter((z) => !z[0].startsWith('_')) // ignore metadata elements
+        .map((z) => z[1]._alias) // pull out the list of IANA IDs for each CLDR zone
+        .filter(Boolean) // CLDR deprecated zones no longer have an IANA ID
+        .flatMap((ids) => ids.split(' ')) // expand all space-delimited IANA IDs for each zone
+        .filter((id) => !['America/Ciudad_Juarez'].includes(id)) // exclude IDs that are too new to be supported
+        .filter((id) => !['Etc/Unknown'].includes(id)); // see https://github.com/tc39/proposal-canonical-tz/pull/25
+
+      // These 4 legacy IDs are in TZDB, in Wikipedia, and accepted by ICU, but they're not in CLDR data.
+      // Not sure where they come from, perhaps hard-coded into ICU, but we'll test them anyway.
+      const missingFromCLDR = ['CET', 'EET', 'MET', 'WET'];
+
+      // All IDs that we know about
+      const ids = [...new Set([...missingFromCLDR, ...cldrIdentifiers, ...Intl.supportedValuesOf('timeZone')])];
+
+      for (const id of ids) {
+        const lower = id.toLowerCase();
+        const upper = id.toUpperCase();
+        equal(ES.GetAvailableNamedTimeZoneIdentifier(id)?.identifier, id);
+        equal(ES.GetAvailableNamedTimeZoneIdentifier(upper)?.identifier, id);
+        equal(ES.GetAvailableNamedTimeZoneIdentifier(lower)?.identifier, id);
+      }
+    });
+    it('Returns canonical IDs', () => {
+      const ids = Intl.supportedValuesOf('timeZone');
+      for (const id of ids) {
+        equal(ES.GetAvailableNamedTimeZoneIdentifier(id).primaryIdentifier, id);
+      }
+      const knownAliases = [
+        ['America/Atka', 'America/Adak'],
+        ['America/Knox_IN', 'America/Indiana/Knox'],
+        ['Asia/Ashkhabad', 'Asia/Ashgabat'],
+        ['Asia/Dacca', 'Asia/Dhaka'],
+        ['Asia/Istanbul', 'Europe/Istanbul'],
+        ['Asia/Macao', 'Asia/Macau'],
+        ['Asia/Thimbu', 'Asia/Thimphu'],
+        ['Asia/Ujung_Pandang', 'Asia/Makassar'],
+        ['Asia/Ulan_Bator', 'Asia/Ulaanbaatar']
+      ];
+      for (const [identifier, primaryIdentifier] of knownAliases) {
+        const record = ES.GetAvailableNamedTimeZoneIdentifier(identifier);
+        equal(record.identifier, identifier);
+        equal(record.primaryIdentifier, primaryIdentifier);
+      }
     });
   });
 });
