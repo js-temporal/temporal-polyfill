0fc0ef8b7c45e8cb446b7a6415d70501d3aef4b2
Philip Chimento <pchimento@igalia.com>
2023-06-16 16:47:35 +0200

    Simplify rounding in Duration.prototype.toString
    toString() can only round the seconds and lower units, so we do not need
    to pass all the units in to RoundDuration - we can only round to seconds
    increments and smaller anyway. This makes it clearer that implementations
    can treat seconds as a single number if they wish.
    
    UPSTREAM_COMMIT=4c2fcafd25e06ba82bf50e270a0f9ef252f1da55

--- lib/duration.ts
+++ lib/duration.ts
@@ -443,29 +443,28 @@ export class Duration implements Temporal.Duration {
       precision,
       'Precision cannot be "minute" because of RangeError above'
     );
-    let { years, months, weeks, days, hours, minutes, seconds, milliseconds, microseconds, nanoseconds } =
-      ES.RoundDuration(
-        GetSlot(this, YEARS),
-        GetSlot(this, MONTHS),
-        GetSlot(this, WEEKS),
-        GetSlot(this, DAYS),
-        GetSlot(this, HOURS),
-        GetSlot(this, MINUTES),
-        GetSlot(this, SECONDS),
-        GetSlot(this, MILLISECONDS),
-        GetSlot(this, MICROSECONDS),
-        GetSlot(this, NANOSECONDS),
-        increment,
-        unit,
-        roundingMode
-      );
+    let { seconds, milliseconds, microseconds, nanoseconds } = ES.RoundDuration(
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      GetSlot(this, SECONDS),
+      GetSlot(this, MILLISECONDS),
+      GetSlot(this, MICROSECONDS),
+      GetSlot(this, NANOSECONDS),
+      increment,
+      unit,
+      roundingMode
+    );
     return ES.TemporalDurationToString(
-      years,
-      months,
-      weeks,
-      days,
-      hours,
-      minutes,
+      GetSlot(this, YEARS),
+      GetSlot(this, MONTHS),
+      GetSlot(this, WEEKS),
+      GetSlot(this, DAYS),
+      GetSlot(this, HOURS),
+      GetSlot(this, MINUTES),
       seconds,
       milliseconds,
       microseconds,
