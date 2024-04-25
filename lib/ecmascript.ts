7c11bf219a1713d4a4fc93fd6f8981684f383605
Philip Chimento <pchimento@igalia.com>
2024-04-25 10:08:27 -0700

    Editorial: Refactor RoundTime to use units table
    There were several issues with RoundTime both in the spec text and the
    reference code.
    
    The reference code used bigints where that was not necessary. The numbers
    in this operation are only ever safe integers. It also used a switch
    statement which was difficult to understand due to its unusual use of
    fallthrough.
    
    The spec text used fractional mathematical values which were correct but
    made it needlessly complicated to implement. Switch to using the same
    algorithm as the reference code previously used, including looking up the
    length of a unit in nanoseconds from the units table.
    
    UPSTREAM_COMMIT=e4b6a9a6b4c613e8851996155eb359b1dbc38b5b

--- lib/ecmascript.ts
+++ lib/ecmascript.ts
@@ -6092,30 +6092,29 @@ export function RoundTime(
   unit: TimeUnitOrDay,
   roundingMode: Temporal.RoundingMode
 ) {
-  let quantity = ZERO;
+  let quantity;
   switch (unit) {
     case 'day':
     case 'hour':
-      quantity = JSBI.BigInt(hour);
-    // fall through
+      quantity = ((((hour * 60 + minute) * 60 + second) * 1000 + millisecond) * 1000 + microsecond) * 1000 + nanosecond;
+      break;
     case 'minute':
-      quantity = JSBI.add(JSBI.multiply(quantity, SIXTY), JSBI.BigInt(minute));
-    // fall through
+      quantity = (((minute * 60 + second) * 1000 + millisecond) * 1000 + microsecond) * 1000 + nanosecond;
+      break;
     case 'second':
-      quantity = JSBI.add(JSBI.multiply(quantity, SIXTY), JSBI.BigInt(second));
-    // fall through
+      quantity = ((second * 1000 + millisecond) * 1000 + microsecond) * 1000 + nanosecond;
+      break;
     case 'millisecond':
-      quantity = JSBI.add(JSBI.multiply(quantity, THOUSAND), JSBI.BigInt(millisecond));
-    // fall through
+      quantity = (millisecond * 1000 + microsecond) * 1000 + nanosecond;
+      break;
     case 'microsecond':
-      quantity = JSBI.add(JSBI.multiply(quantity, THOUSAND), JSBI.BigInt(microsecond));
-    // fall through
+      quantity = microsecond * 1000 + nanosecond;
+      break;
     case 'nanosecond':
-      quantity = JSBI.add(JSBI.multiply(quantity, THOUSAND), JSBI.BigInt(nanosecond));
+      quantity = nanosecond;
   }
   const nsPerUnit = NS_PER_TIME_UNIT.get(unit);
-  const rounded = RoundNumberToIncrement(quantity, JSBI.BigInt(nsPerUnit * increment), roundingMode);
-  const result = JSBI.toNumber(JSBI.divide(rounded, JSBI.BigInt(nsPerUnit)));
+  const result = RoundJSNumberToIncrement(quantity, nsPerUnit * increment, roundingMode) / nsPerUnit;
   switch (unit) {
     case 'day':
       return { deltaDays: result, hour: 0, minute: 0, second: 0, millisecond: 0, microsecond: 0, nanosecond: 0 };
