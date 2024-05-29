90727515ee29547a6ae3a36b2ba78d4330939f27
gimy <b.afatah@yahoo.com>
2024-05-29 13:15:13 +0800

    fix: use a simpler patch
    UPSTREAM_COMMIT=008e4b782a5473001e722c69f0eb7a9b8a7baa64

--- lib/calendar.ts
+++ lib/calendar.ts
@@ -1572,9 +1572,9 @@ abstract class IslamicBaseHelper extends HelperBase {
   override reviseIntlEra<T extends Partial<EraAndEraYear>>(calendarDate: T /*, isoDate: IsoYMD */): T {
     let { era, eraYear } = calendarDate;
     // Chrome for Android as of v 142.0.6367.179 mishandled the era option in Intl.DateTimeFormat
-    // and returned 'bc' instead of 'ah'. This code corrects that.
+    // and returned 'bc' instead of 'ah'. This code corrects that and any possible future errors.
     // see https://issues.chromium.org/issues/40856332
-    if (era === 'before-christ' || era === 'bc' || era === 'b') era = 'ah';
+    era = 'ah';
     return { era, eraYear } as T;
   }
   estimateIsoDate(calendarDate: CalendarYMD) {
