510adeba0f4df2ad42033ed5d322c8df7edc76e7
Philip Chimento <pchimento@igalia.com>
2024-07-15 12:35:41 -0700

    Normative: Disallow rounding to increment while balancing to calendar unit
    The corner case of rounding to a >1 increment of a calendar smallest unit
    while simultaneously balancing to a larger calendar unit is ambiguous.
    This use case was probably never considered.
    
    const d1 = Temporal.Duration.from({months: 9});
    d1.round({
      relativeTo: '2024-01-01',
      largestUnit: 'years',
      smallestUnit: 'months',
      roundingIncrement: 8,
      roundingMode: 'ceil',
    });  // => 1 year? 1 year 4 months?
    
    This never came up in real-world usage. Disallow it explicitly, to leave
    space for a future proposal if it ever comes up.
    
    Closes: #2902
    
    UPSTREAM_COMMIT=74aa2309db9e5edfe520770fb2e6758a56f8ba8f

--- lib/duration.ts
+++ lib/duration.ts
@@ -263,6 +263,13 @@ export class Duration implements Temporal.Duration {
     } as { [k in Temporal.DateTimeUnit]?: number };
     const maximum = maximumIncrements[smallestUnit];
     if (maximum !== undefined) ES.ValidateTemporalRoundingIncrement(roundingIncrement, maximum, false);
+    if (
+      roundingIncrement > 1 &&
+      (ES.IsCalendarUnit(smallestUnit) || smallestUnit === 'day') &&
+      largestUnit !== smallestUnit
+    ) {
+      throw new RangeError('For calendar units with roundingIncrement > 1, use largestUnit = smallestUnit');
+    }
 
     let norm = TimeDuration.normalize(hours, minutes, seconds, milliseconds, microseconds, nanoseconds);
 
