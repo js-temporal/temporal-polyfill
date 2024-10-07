c3642276bfe4a0c1aaa95d1df90dad4ce0dd65a1
Philip Chimento <pchimento@igalia.com>
2024-10-07 11:26:34 -0700

    Editorial: Return ISO Date-Time Record from SystemDateTime
    This avoids creation of an unobservable Temporal.PlainDateTime object, and
    is also simpler to read.
    
    See: #2949
    
    UPSTREAM_COMMIT=1a23f2c9f2c044ea0053b064e30bb44a5e200cf0

--- lib/now.ts
+++ lib/now.ts
@@ -2,8 +2,11 @@ import { ObjectDefineProperty, SymbolToStringTag } from './primordials';
 
 import * as ES from './ecmascript';
 import { GetIntrinsic } from './intrinsicclass';
-import { GetSlot, ISO_DATE_TIME } from './slots';
 import type { Temporal } from '..';
+
+function SystemDateTime(timeZone: string) {
+  return ES.GetISODateTimeFor(timeZone, ES.SystemUTCEpochNanoSeconds());
+}
 
 const instant: (typeof Temporal.Now)['instant'] = () => {
   const Instant = GetIntrinsic('%Temporal.Instant%');
@@ -11,7 +14,7 @@ const instant: (typeof Temporal.Now)['instant'] = () => {
 };
 const plainDateTimeISO: (typeof Temporal.Now)['plainDateTimeISO'] = (temporalTimeZoneLike = ES.DefaultTimeZone()) => {
   const timeZone = ES.ToTemporalTimeZoneIdentifier(temporalTimeZoneLike);
-  const isoDateTime = ES.GetISODateTimeFor(timeZone, ES.SystemUTCEpochNanoSeconds());
+  const isoDateTime = SystemDateTime(timeZone);
   return ES.CreateTemporalDateTime(isoDateTime, 'iso8601');
 };
 const zonedDateTimeISO: (typeof Temporal.Now)['zonedDateTimeISO'] = (temporalTimeZoneLike = ES.DefaultTimeZone()) => {
@@ -19,10 +22,14 @@ const zonedDateTimeISO: (typeof Temporal.Now)['zonedDateTimeISO'] = (temporalTim
   return ES.CreateTemporalZonedDateTime(ES.SystemUTCEpochNanoSeconds(), timeZone, 'iso8601');
 };
 const plainDateISO: (typeof Temporal.Now)['plainDateISO'] = (temporalTimeZoneLike = ES.DefaultTimeZone()) => {
-  return ES.CreateTemporalDate(GetSlot(plainDateTimeISO(temporalTimeZoneLike), ISO_DATE_TIME).isoDate, 'iso8601');
+  const timeZone = ES.ToTemporalTimeZoneIdentifier(temporalTimeZoneLike);
+  const isoDateTime = SystemDateTime(timeZone);
+  return ES.CreateTemporalDate(isoDateTime.isoDate, 'iso8601');
 };
 const plainTimeISO: (typeof Temporal.Now)['plainTimeISO'] = (temporalTimeZoneLike = ES.DefaultTimeZone()) => {
-  return ES.CreateTemporalTime(GetSlot(plainDateTimeISO(temporalTimeZoneLike), ISO_DATE_TIME).time);
+  const timeZone = ES.ToTemporalTimeZoneIdentifier(temporalTimeZoneLike);
+  const isoDateTime = SystemDateTime(timeZone);
+  return ES.CreateTemporalTime(isoDateTime.time);
 };
 const timeZoneId: (typeof Temporal.Now)['timeZoneId'] = () => {
   return ES.DefaultTimeZone();
