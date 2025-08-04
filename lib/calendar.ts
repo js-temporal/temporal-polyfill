3f5f7b8d585673a7d9cecce6cfe725bd23739f79
Philip Chimento <pchimento@igalia.com>
2025-08-04 07:26:38 -0700

    Update reference code to match editorial change
    For the weeks and days part, we already used a shortcut instead of
    looping, so we keep it the same as before. For the years and months
    calculation, we change to the new definition of ISODateSurpasses.
    
    UPSTREAM_COMMIT=f6e78d07dea1daf9e12c8c987cc12d4c9a2b2583

--- lib/calendar.ts
+++ lib/calendar.ts
@@ -78,7 +78,27 @@ function calendarDateWeekOfYear(id: BuiltinCalendarId, isoDate: ISODate): { week
   return { week: woy, year: yow };
 }
 
-function ISODateSurpasses(sign: -1 | 0 | 1, y1: number, m1: number, d1: number, isoDate2: ISODate) {
+function ISODateSurpasses(
+  sign: -1 | 0 | 1,
+  baseDate: ISODate,
+  isoDate2: ISODate,
+  years: number,
+  months: number,
+  weeks: number,
+  days: number
+) {
+  const yearMonth = ES.BalanceISOYearMonth(baseDate.year + years, baseDate.month + months);
+  let y1 = yearMonth.year;
+  let m1 = yearMonth.month;
+  let d1 = baseDate.day;
+  if (weeks !== 0 || days !== 0) {
+    const regulatedDate = ES.RegulateISODate(y1, m1, d1, 'constrain');
+    ({
+      year: y1,
+      month: m1,
+      day: d1
+    } = ES.BalanceISODate(regulatedDate.year, regulatedDate.month, regulatedDate.day + 7 * weeks + days));
+  }
   if (y1 !== isoDate2.year) {
     if (sign * (y1 - isoDate2.year) > 0) return true;
   } else if (m1 !== isoDate2.month) {
@@ -199,25 +219,22 @@ impl['iso8601'] = {
 
     let years = 0;
     let months = 0;
-    let intermediate;
     if (largestUnit === 'year' || largestUnit === 'month') {
       // We can skip right to the neighbourhood of the correct number of years,
       // it'll be at least one less than two.year - one.year (unless it's zero)
       let candidateYears = two.year - one.year;
       if (candidateYears !== 0) candidateYears -= sign;
       // loops at most twice
-      while (!ISODateSurpasses(sign, one.year + candidateYears, one.month, one.day, two)) {
+      while (!ISODateSurpasses(sign, one, two, candidateYears, 0, 0, 0)) {
         years = candidateYears;
         candidateYears += sign;
       }
 
       let candidateMonths = sign;
-      intermediate = ES.BalanceISOYearMonth(one.year + years, one.month + candidateMonths);
       // loops at most 12 times
-      while (!ISODateSurpasses(sign, intermediate.year, intermediate.month, one.day, two)) {
+      while (!ISODateSurpasses(sign, one, two, years, candidateMonths, 0, 0)) {
         months = candidateMonths;
         candidateMonths += sign;
-        intermediate = ES.BalanceISOYearMonth(intermediate.year, intermediate.month + sign);
       }
 
       if (largestUnit === 'month') {
@@ -226,7 +243,7 @@ impl['iso8601'] = {
       }
     }
 
-    intermediate = ES.BalanceISOYearMonth(one.year + years, one.month + months);
+    const intermediate = ES.BalanceISOYearMonth(one.year + years, one.month + months);
     const constrained = ES.ConstrainISODate(intermediate.year, intermediate.month, one.day);
 
     let weeks = 0;
