2a93b7687345338df24b9061c420ad74e7a2724b
Philip Chimento <pchimento@igalia.com>
2025-09-19 10:37:56 -0700

    Polyfill: Avoid RegExp in ParseMonthCode
    The RegExp was showing up on profiles. This is not the root cause of the
    slowness in #3153, but it noticeably helps.
    
    A RegExp is kind of overkill anyway, since the MonthCode grammar is very
    simple and the different elements occur at fixed indices in the string.
    Replacing the month code RegExp with very simple string-indexing code
    makes it go a lot faster.
    
    Adds a unit test suite for this file.
    
    See: #3153
    
    Co-Authored-By: Richard Gibson <richard.gibson@gmail.com>
    
    UPSTREAM_COMMIT=a23f1cfdcc4fa735e3ecf323326238f5b48655cc

--- lib/monthcode.ts
+++ lib/monthcode.ts
@@ -1,14 +1,21 @@
 import { ToPrimitiveRequireString } from './primitive';
-import { monthCode as MONTH_CODE_REGEX } from './regex';
+
+const digitsForMonthNumber = Array.from({ length: 100 }, (_, i) => (i < 10 ? `0${i}` : `${i}`));
 
 export function ParseMonthCode(argument: unknown) {
   const value = ToPrimitiveRequireString(argument);
-  const match = MONTH_CODE_REGEX.exec(value);
-  if (!match) throw new RangeError(`bad month code ${value}; must match M01-M99 or M00L-M99L`);
-  return {
-    monthNumber: +(match[1] ?? match[3] ?? match[5]),
-    isLeapMonth: (match[2] ?? match[4] ?? match[6]) === 'L'
-  };
+  const digits = value.slice(1, 3);
+  const monthNumber = digits.length === 2 ? +digits : -1; // -1 ensures failure
+  const isLeapMonth = value.length === 4;
+  if (
+    !(monthNumber >= 0) ||
+    digits !== digitsForMonthNumber[monthNumber] ||
+    value[0] !== 'M' ||
+    (isLeapMonth ? value[3] !== 'L' : value.length !== 3 || monthNumber === 0)
+  ) {
+    throw new RangeError(`bad month code ${value}; must match M01-M99 or M00L-M99L`);
+  }
+  return { monthNumber, isLeapMonth };
 }
 
 export function CreateMonthCode(monthNumber: number, isLeapMonth: boolean) {
