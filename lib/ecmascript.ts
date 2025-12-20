a5ed8f18e35c14b12b429d2aab3fa17b17ded9fb
Tim Chevalier <tjc@igalia.com>
2025-12-19 20:43:29 -0800

    Editorial: Change check to assertion in IsValidDuration
    This shouldn't be called with infinity as the value of any field,
    as far as I can tell.
    
    UPSTREAM_COMMIT=e65f411f1cfceb3a54c30139a3d734f7efa7f04b

--- lib/ecmascript.ts
+++ lib/ecmascript.ts
@@ -3278,7 +3278,7 @@ export function RejectDuration(
   const fields = [y, mon, w, d, h, min, s, ms, µs, ns];
   for (let index = 0; index < fields.length; index++) {
     const prop = fields[index];
-    if (prop === Infinity || prop === -Infinity) throw new RangeError('infinite values not allowed as duration fields');
+    assert(prop !== Infinity && prop !== -Infinity, 'infinite values not allowed as duration fields');
     if (prop !== 0) {
       const propSign = prop < 0 ? -1 : 1;
       if (sign !== 0 && propSign !== sign) throw new RangeError('mixed-sign values not allowed as duration fields');
