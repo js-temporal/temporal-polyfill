1401f0c05b0367566f0482bd78ca611d5899e120
Tim Chevalier <tjc@igalia.com>
2026-01-14 11:37:40 -0800

    Polyfill: Check that time is not start-of-day in ToTemporalInstant (#3230)
    The TemporalInstantString production requires a Time, so `time` will
    never be `start-of-day` in this code.
    
    UPSTREAM_COMMIT=23220c07ea45b6efcf10e21c6ae13a8c5af7c987

--- lib/ecmascript.ts
+++ lib/ecmascript.ts
@@ -1523,14 +1523,8 @@ export function ToTemporalInstant(itemParam: InstantParams['from'][0]) {
     item = itemParam;
   }
   const { year, month, day, time, offset, z } = ParseTemporalInstantString(RequireString(item));
-  const {
-    hour = 0,
-    minute = 0,
-    second = 0,
-    millisecond = 0,
-    microsecond = 0,
-    nanosecond = 0
-  } = time === 'start-of-day' ? {} : time;
+  assert(time !== 'start-of-day', 'Instant string must include a time');
+  const { hour, minute, second, millisecond, microsecond, nanosecond } = time;
 
   // ParseTemporalInstantString ensures that either `z` is true or or `offset` is non-undefined
   const offsetNanoseconds = z ? 0 : ParseDateTimeUTCOffset(castExists(offset));
