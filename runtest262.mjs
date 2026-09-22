f452985c25d40546cb81fb646bfdb152559e21b6
Philip Chimento <pchimento@igalia.com>
2026-09-22 09:47:09 -0700

    Remove Node 23 from test matrix
    Only the latest odd version is supported by Node, 23 is not LTS.

--- runtest262.mjs
+++ runtest262.mjs
@@ -32,10 +32,10 @@ yargs(hideBin(process.argv))
       if (nodeVersion < 16) expectedFailureFiles.push('test/expected-failures-before-node16.txt');
       if (nodeVersion < 20) expectedFailureFiles.push('test/expected-failures-before-node20.txt');
       if (nodeVersion < 22) expectedFailureFiles.push('test/expected-failures-before-node22.txt');
-      if (nodeVersion < 23) expectedFailureFiles.push('test/expected-failures-before-node23.txt');
+      if (nodeVersion < 24) expectedFailureFiles.push('test/expected-failures-before-node24.txt');
       // Eventually this should be fixed and this condition should be updated.
       if (nodeVersion >= 18 && nodeVersion < 26) expectedFailureFiles.push('test/expected-failures-cldr42.txt');
-      if (nodeVersion >= 20 && nodeVersion !== 23) expectedFailureFiles.push('test/expected-failures-cldr48.txt');
+      if (nodeVersion >= 20) expectedFailureFiles.push('test/expected-failures-cldr48.txt');
 
       // As we migrate commits from proposal-temporal, remove expected failures from here.
       expectedFailureFiles.push('test/expected-failures-todo-migrated-code.txt');
